d4303797172b694c7389d1773d99c00ff7d61ab2
melissa-murphy <melissamurphy.ca@gmail.com>
2019-04-05 09:54:24 -0700

    Stkon part 3 Uni. Filters, but not just uni name

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -95,9 +95,12 @@ console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+let uni = [];
 console.log(uni);
-
+uni = graduates.filter(val => {
+  return val.university = 'uni';
+})
+console.log(uni);
 
 // ==== ADVANCED Array Methods ====
 
